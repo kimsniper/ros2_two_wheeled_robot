72183190b0c9655d75c5c19c6fe3e3592a5f4824
kimsniper <kim1991mail@gmail.com>
2026-05-23 17:38:31 +0800

    removed code

--- ros2_ws/src/robot_rl/launch/robot_rl.launch.py
+++ ros2_ws/src/robot_rl/launch/robot_rl.launch.py
@@ -10,14 +10,6 @@ def generate_launch_description():
         output='screen'
     )
 
-    rl_episode_manager_node = Node(
-        package='robot_rl',
-        executable='rl_episode_manager',
-        name='rl_episode_manager',
-        output='screen'
-    )
-
     return LaunchDescription([
         rl_pid_agent_node,
-        rl_episode_manager_node
     ])
